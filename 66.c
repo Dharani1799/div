372cf6fbd6ac1cde03d949087d1ed128ce36fda6
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-13 00:03:46 +0530

    66.c

---
+++ 66.c
@@ -0,0 +1,18 @@
+#include<stdio.h>  
+void main(){    
+int n,i,m=0,f=0;    
+   
+scanf("%d",&n);    
+m=n/2;    
+for(i=2;i<=m;i++)    
+{    
+if(n%i==0)    
+{    
+printf("yes");    
+f=1;    
+break;    
+}    
+}    
+if(f==0)    
+printf("no");     
+ }    
