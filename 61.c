eb0cceb73711c7c549a7f80ead9f672a813e2582
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-12 23:42:06 +0530

    61.c

---
+++ 61.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void main()
+{
+    char a[20];
+    int n,i;
+    scanf("%s",a);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        printf("%c",a[i]);
+    }
+    
+}
