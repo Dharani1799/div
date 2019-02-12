2a02678f10c484697e07df9262aab039b51a2e3d
Dharani1799 <46838677+Dharani1799@users.noreply.github.com>
2019-02-13 00:31:39 +0530

    173.c

---
+++ 173.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdio.h>
+
+void main() {
+	int l,r,s;
+	scanf("%d %d %d",&l,&s,&r);
+	
+	if(s>l&&s<r)
+	{
+		
+		printf("yes");
+	}
+	else
+	{
+		printf("no");
+	}
+
+}
